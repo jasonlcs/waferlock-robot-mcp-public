2200d87018c55391a8e6838eeba6ced8fac8e830
JasonLcs <jasonlcs@gmail.com>
2025-10-28 13:42:08 +0800

    Refactor: Remove all AWS dependencies - pure HTTP API proxy (15 tools)

--- src/services/vectorIndex/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './EmbeddingService';
-export * from './HNSWIndexManager';
-export * from './MetadataStore';
-export * from './ProgressTracker';
-export * from './CheckpointManager';
-export * from './JobLockManager';
-export * from './VectorIndexService';
